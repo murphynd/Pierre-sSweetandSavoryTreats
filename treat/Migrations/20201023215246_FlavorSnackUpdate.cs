7a1b4387bb523e31b441768f1e3acd27769a2179
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-10-23 15:04:33 -0700

    Views: Flavor/Snacks: Details

---
+++ treat/Migrations/20201023215246_FlavorSnackUpdate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Treat.Migrations
+{
+    public partial class FlavorSnackUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
