b7ba4b19982fe9af4c57b36c879ac56a837134eb
Natalie Murphy <Nataliemurphy500@gmail.com>
2020-10-23 09:20:02 -0700

    Framework: Creates skeleton for project

---
+++ treat/program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Treat
+{
+    public class Program
+  {
+    public static void Main(string[] args)
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+        InitializeDatabase(host);
+
+      host.Run();
+    }
+    
